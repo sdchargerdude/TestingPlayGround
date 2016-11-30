900aa927a9274ae9a1ad5892fee84757ae732012
Tim Lavato <tlavato@massageenvy.com>
2016-11-30 06:34:37 -0700

    Initial Checkin

---
+++ Compare-NET-Objects/IntroNunitTests/MyFirstTestClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace IntroNunitTests
+{
+    [TestFixture]
+    public class MyFirstTestClass
+    {
+        [Test]
+        public void PositiveTest()
+        {
+            int x = 7;
+            int y = 7;
+
+            Assert.AreEqual(x,y);
+        }
+
+        [Test]
+        public void NegativeTest()
+        {
+            if (true)
+                Assert.Fail("This is a Failure,Crap");
+        }
+
+        [Test]
+        public void ExpectedExceptionTest()
+        {
+            Assert.Throws(typeof(NotSupportedException), () =>
+            {
+                throw new NotSupportedException();
+            });
+        }
+    }
+}
